55041146bf0f8cb8d46134255de041283bb32fad
devGojo <ra38834258851@acaonsfatima.org.br>
2023-10-30 08:56:14 -0300

    parte amarela feita

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import smirk from '../src/assets/images/bad-yellow.png';
 import pix from '../src/assets/images/pix.png';
 import slogann from '../src/assets/images/yellow bad 1.png';
-import silhueta from '../src/assets/images/silhueta.png';
+import silhueta from '../src/assets/images/profi.png';
 import lupa from '../src/assets/images/LUPA.png';
 import carrinho from '../src/assets/images/carrinho.png';
 import vector from '../src/assets/images/Vector 32.png';
@@ -48,20 +48,48 @@ function App() {
       <img src={vector} className='vector'/>
 
         <h1 className='check'>CHECKOUT</h1>
+
+     <div className='mãe'>
+      <div className='quadrado'>
         <div className='yellow'>
           <h1 className='method'>Selecione o Método de pagamento</h1>
           </div>
+
           <div className='calculos'>
-              <div>
-               <img src={pix} className='PIX'/> <h1 className='pex'>PIX</h1> <h2 className='medi'>APROVAÇAO  IMEDIATA</h2>
-               </div>
-               <div>
-               <img src={cartão} className='card'/> <h1 className='car'>CARTÃO DE DEBITO</h1>
-               </div>
-               <div>
-               <img src={cartão} className='cart'/> <h1 className='cre'>CARTÃO DE CRÉDITO</h1>
-               </div>
+            <div className='piquis'>
+                <div className='pix1'>
+                  <img src={pix} className='PIX'/>
+                </div>
+                <div className='pix2'>
+                  <h2>PIX</h2> 
+                  <h4>APROVAÇAO IMEDIATA</h4>
+                </div>
             </div>
+
+            <div className='debito'>
+              <div className='debito1'>
+                <img src={cartão} className='card'/>
+              </div>
+              <div className='debito2'>
+                <h2 className='car'>CARTÃO DE DEBITO</h2>
+              </div>
+            </div>
+               <div className='credito'>
+                  <div className='credito1'>
+                    <img src={cartão} className='cart'/>
+                  </div>
+                  <div className='credito2'>
+                    <h2 className='cre'>CARTÃO DE CRÉDITO</h2>
+                  </div>
+               </div>
+          </div>
+      </div>
+            <div className='checkin'>
+              <div className='resumo'>
+                <h1>resumo do pedido</h1>
+              </div>
+           </div>
+</div>
         </section>
     </main>
   );
